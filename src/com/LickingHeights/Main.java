9665f80308c20c090f0e962f3b5474ac04f6c735
Janae Anderson <janae.anderson.2022@student.lhschools.org>
2020-03-04 13:37:38 -0500

    Intellij commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,6 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Phase 1
---
+++ src/com/LickingHeights/src/com/LickingHeights/Main.java
@@ -0,0 +1,125 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// Phase 1
+
+        String girlName;
+        String food;
+        String place;
+        String state;
+        String verb;
+        String color;
+        String restaurant;
+        String time;
+        String adjective;
+        String gender;
+        String pronoun;
+        String animal;
+        String city;
+        String bodyPart;
+        String country;
+        String number;
+        String season;
+        String celebrity;
+        String planet;
+        String relative;
+        Scanner Keyboard;
+
+        // Phase 2
+        // Initializing
+
+        Keyboard = new Scanner ( System.in );
+
+
+
+        System.out.println("Give me a girls name");
+        girlName = Keyboard.nextLine();
+
+        System.out.println("Give me your favorite food");
+        food = Keyboard.nextLine();
+
+        System.out.println("Give me a city");
+        city = Keyboard.nextLine();
+
+        System.out.println("Give me your favorite state");
+        state = Keyboard.nextLine();
+
+        System.out.println("Give me a verb");
+        verb = Keyboard.nextLine();
+
+        System.out.println("Name your favorite color");
+        color = Keyboard.nextLine();
+
+        System.out.println("Your favorite restaurant");
+        restaurant = Keyboard.nextLine();
+
+        System.out.println("Give me a time of day");
+        time = Keyboard.nextLine();
+
+        System.out.println("Give me an adjective");
+        adjective = Keyboard.nextLine();
+
+        System.out.println("any gender");
+        gender = Keyboard.nextLine();
+
+        System.out.println("Any pronoun");
+        pronoun = Keyboard.nextLine();
+
+        System.out.println("Name your favorite animal");
+        animal = Keyboard.nextLine();
+
+        System.out.println("Give me any place");
+        place = Keyboard.nextLine();
+
+        System.out.println("Name any bodyPart");
+        bodyPart = Keyboard.nextLine();
+
+        System.out.println("Nmae your favorite country");
+        country = Keyboard.nextLine();
+
+        System.out.println("give me your favorite number");
+        number = Keyboard.nextLine();
+
+        System.out.println("Give me your favorite season");
+        season = Keyboard.nextLine();
+
+        System.out.println("Who is your favorite celebrity");
+        celebrity = Keyboard.nextLine();
+
+        System.out.println("Name a planet");
+        planet = Keyboard.nextLine();
+
+        System.out.println("Give me a type of relative");
+        relative = Keyboard.nextLine();
+
+
+        //Phase 3
+
+        System.out.println("There was a girl named "+ girlName +" . \n");
+        System.out.println("She loved to eat "+ food +" so much . \n");
+        System.out.println("She even at them at "+ place +" . \n ");
+        System.out.println("One day, her family decided to move to "+ state +" . \n");
+        System.out.println(" She was so upset that she "+ verb +" away . \n");
+        System.out.println("While she was out, she saw a bright "+ color +" sign and knew it was "+ restaurant +" . \n ");
+        System.out.println("It was "+ time+" and she was very "+ adjective +" . \n ");
+        System.out.println("While eating she met a "+ gender +" and they ate together . \n ");
+        System.out.println(" "+ pronoun +" encountered a stray "+ animal +" decided to bring it along . \n");
+        System.out.println("Soon enough they were in "+ city +" and had scratches all over their "+ bodyPart +" from their long journey . \n");
+        System.out.println(" "+ country +" was very beautiful and they received "+ number +" letters. \n ");
+        System.out.println("It was "+ season +" and "+ celebrity +" was going to "+ planet +" and they were invited . \n ");
+        System.out.println("She decided to bring her "+ relative +" with her because they were close and they lived happily ever after . The end . \n");
+
+
+
+
+
+
+
+
+
+    }
+}
